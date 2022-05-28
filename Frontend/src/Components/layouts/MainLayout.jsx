4b6e104fb92970d44169be05ba1821be2cb7a34a
patthanan <yok.patthanan@gmail.com>
2022-05-28 17:45:37 +0700

    remove unused import

--- Frontend/src/Components/layouts/MainLayout.jsx
+++ Frontend/src/Components/layouts/MainLayout.jsx
@@ -3,9 +3,6 @@ import Header from "../shared/Header"
 import {BrowserRouter as Router,Routes, Route, Link} from "react-router-dom";
 import Home from "../../Home";
 import Search from "../../Pages/Search";
-import SearchBar from "../shared/SearchBar";
-
-import {Container, Row, Col} from 'react-bootstrap';
 
 
 function MainLayout() {
