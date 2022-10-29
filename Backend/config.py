1a34e92b2bf043f70fda70c8fccea304e700bf9d
patthanan <yok.patthanan@gmail.com>
2022-10-29 23:50:04 +0700

    upload

--- Backend/config.py
+++ Backend/config.py
@@ -4,7 +4,7 @@ class BaseConfig(object):
     SECRET_KEY = 'hO6rCflxLF9SfiU2fFy3Q2LBlQ4d1bhUeDNQZmfqoEDLdg4n0nwlYLqAxitN?mAF=Xr?XZpPaCR1pM=8tdjqtpONxLJ!tCvhX8TPA//4RepWNbMrry2mUIi-?3lL=yG8j?QOs1RGf5TW?39GMZvibHujwW9U3k5Mgjm1M/1ZGk9!toLGMacVw!UUGYLuLp3YvfbvbTsHJ-W2Vi72VOHSvkbyC9sJEUSd6w!vsP4mckBlUjoLkzr=TgS7PyB8qS3C'
     SECURITY_PASSWORD_SALT = 'P@tthanan'
     
-    UPLOAD_FOLDER = "C:/test_uploads"
+    UPLOAD_FOLDER = "/Users/patthanan/Desktop/test_uploads"
     ALLOWED_EXTENSIONS = ('txt', 'pdf')
 
     AWS_ACCESS_KEY_ID='AKIAVRZ3UCNLB7SUBNO2'
